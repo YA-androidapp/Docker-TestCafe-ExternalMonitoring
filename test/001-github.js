b8e1e5a5889ad3c4b5fabbaf786b61ab3401ca6f
YA <YA-androidapp@users.noreply.github.com>
2020-06-08 08:21:56 +0900

    Add 検索

--- test/001-github.js
+++ test/001-github.js
@@ -11,3 +11,13 @@ test("本文にGitHubを含まない", async (t) => {
   const body = Selector("body");
   await t.expect(body.innerText).notContains("GitHub");
 });
+
+test("検索", async (t) => {
+  await t.typeText(Selector("input[name=q]"), "Yumura").pressKey("enter");
+
+  const list = Selector("ul.repo-list");
+  const listitem = list.child(0);
+  const listitemExists = listitem.exists;
+  await t.expect(listitemExists).ok();
+  await t.expect(listitem.innerText).contains("YA-androidapp/Yumura");
+});
